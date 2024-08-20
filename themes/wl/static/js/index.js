4bc1a032836db73cab7a51288739600dc7c35482
Simerl <33984591+SimeonLukas@users.noreply.github.com>
2024-08-20 22:15:07 +0200

    update

--- themes/wl/static/js/index.js
+++ themes/wl/static/js/index.js
@@ -6,3 +6,14 @@ function toggleMenu() {
         x.style.display = "block";
     }
 }
+
+function parallax() {
+    if (document.getElementById("parallax")) {
+      var img = document.getElementsByTagName("img");
+    for (var i = 0; i < img.length; i++) {
+        img[i].style.transform = "translateY(" + window.scrollY * 0.2 * -i + "px)";
+    console.log(i);
+    }
+    }
+}
+window.addEventListener("scroll", parallax);
